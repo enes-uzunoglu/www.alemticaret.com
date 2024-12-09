1ff40150d71c2223b54832131bd45b534827f84d
enes-uzunoglu <developer.enesuzunoglu@gmail.com>
2024-12-10 02:40:01 +0300

    Defines Global Properties and Headings

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'], // h1-h2-h3-h4-h5-h6
+      },
+      colors: {
+        primaryColor: '#23A6F0',
+        secondaryColor: '#2A7CC7',
+        primaryTextColor: '#252B42', // h1-h2-h3-h4-h5-h6
+        secondaryTextColor: '#737373'
+      },
+    },
   },
   plugins: [],
 }
